11176efccdbcb7033b60fed5ed1570ceef9766f3
Josh S <josh.stirland14@gmail.com>
2024-02-27 17:27:24 +0000

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TweakCake",
-    version="1.0.4.2",
+    version="1.0.4.3",
     url="https://github.com/tweesecake/tweakcake",
     license="Apache Software License",
     author="Andrey Kislyuk",
